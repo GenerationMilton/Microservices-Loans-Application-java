dd9018af90a2753bb147a634d761400d63890b6a
Milton Muñoz <livemilton@gmail.com>
2024-08-30 20:45:11 -0500

    about docker and spring boot profile

--- src/main/java/com/livemilton/loans/LoansApplication.java
+++ src/main/java/com/livemilton/loans/LoansApplication.java
@@ -1,40 +1,29 @@
 package com.livemilton.loans;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.livemilton.loans.dto.LoansContactInfoDto;
+
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 
 @SpringBootApplication
-/*@ComponentScans({ @ComponentScan("com.livemilton.loans.controller") })
-@EnableJpaRepositories("com.livemilton.loans.repository")
-@EntityScan("com.livemilton.loans.model")*/
+/*
+ * @ComponentScans({ @ComponentScan("com.livemilton.loans.controller") })
+ * 
+ * @EnableJpaRepositories("com.livemilton.loans.repository")
+ * 
+ * @EntityScan("com.livemilton.loans.model")
+ */
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Loans microservice REST API Documentation",
-				description = "Livemilton Loans microservice REST API Documentation",
-				version = "v1",
-				contact = @Contact(
-						name = "Milton Munoz",
-						email = "livemilton@hotmail.com",
-						url = "https://www.livemilton.com"
-				),
-				license = @License(
-						name = "Apache 2.0",
-						url = "https://www.livemilton.com"
-				)
-		),
-		externalDocs = @ExternalDocumentation(
-				description = "Livemilton Loans microservice REST API Documentation",
-				url = "https://www.livemilton.com/swagger-ui.html"
-		)
-)
+@EnableConfigurationProperties(value = { LoansContactInfoDto.class })
+@OpenAPIDefinition(info = @Info(title = "Loans microservice REST API Documentation", description = "Livemilton Loans microservice REST API Documentation", version = "v1", contact = @Contact(name = "Milton Munoz", email = "livemilton@hotmail.com", url = "https://www.livemilton.com"), license = @License(name = "Apache 2.0", url = "https://www.livemilton.com")), externalDocs = @ExternalDocumentation(description = "Livemilton Loans microservice REST API Documentation", url = "https://www.livemilton.com/swagger-ui.html"))
 public class LoansApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/livemilton/loans/repository/LoansRepository.java
+++ src/main/java/com/livemilton/loans/repository/LoansRepository.java
@@ -1,13 +1,13 @@
 package com.livemilton.loans.repository;
 
+import java.util.Optional;
 
-import com.livemilton.loans.entity.Loans;
-import jakarta.transaction.Transactional;
-import jakarta.validation.constraints.Pattern;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 
-import java.util.Optional;
+import com.livemilton.loans.entity.Loans;
+
+import jakarta.transaction.Transactional;
 
 public interface LoansRepository extends JpaRepository<Loans, Long> {
 
